d0b620892c47913a851ab85171e5f8a7d9dc2614
tuanaakyildiz <139168702+tuanaakyildiz@users.noreply.github.com>
2026-01-10 23:10:56 +0300

    planning lstm_regression

---
+++ lstm_regression.py
@@ -0,0 +1,27 @@
+# import liblaries
+
+
+
+#load yelp dataset from huggingface https://huggingface.co/datasets/Yelp/yelp_review_full
+
+
+
+
+#data preprocessing
+
+
+
+
+#LSTM based regression model
+
+
+
+
+
+#model compile and training 
+
+
+
+
+
+# visulize training  loss graphs and save model 
